61f0881398f874331e7a5d2ba04ff88c30a93a91
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-05-22 13:08:58 +0530

    Add files via upload

---
+++ Strings/reverseString.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char A[]="python";
+	char B[7];
+	int i=0,j;
+	for (i;A[i]!='\0';i++){
+
+	}
+	i=i-1;
+	for (j=0;i>=0;i--;j++){
+		B[j]=A[i];
+	}
+	B[j]='\0';
+	printf("%s\n",B );
+
+}
